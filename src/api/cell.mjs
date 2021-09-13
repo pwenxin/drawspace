15bec40c4ee373dcc5591186dc6bdcd97c1be762
Page <1243202465@qq.com>
2021-09-13 22:00:31 +0800

    fix cell

--- src/api/cell.mjs
+++ src/api/cell.mjs
@@ -1,15 +1,18 @@
 export class Cell {
 
     fill = '#fff';
-    stroke_w = '1';
-    
-    constructor(x, y, w, h, fill, stroke_w) {
+    stroke_w = '1'
+    border_r = '0';
+    object = null;
+
+    constructor(x, y, w, h, fill, stroke_w, border_r) {
         this.x = x;
         this.y = y;
         this.w = w;
         this.h = h;
         this.fill = fill;
         this.stroke_w = stroke_w;
+        this.border_r = border_r
     }
 
     setGroup(group) {
@@ -18,15 +21,32 @@ export class Cell {
 
     draw() {
         let cell = document.createElementNS("http://www.w3.org/2000/svg", 'rect');
-        cell.setAttribute('width', this.h);
+
+        cell.setAttribute('x', this.x);
+        cell.setAttribute('y', this.y);
+        cell.setAttribute('width', this.w);
         cell.setAttribute('height', this.h);
-        cell.style.position = 'absolute';
-        cell.style.top = this.y;
-        cell.style.left = this.x;
-        cell.style.left = this.x;
         cell.style.fill = this.fill;
         cell.style.strokeWidth = this.stroke_w;
+        cell.setAttribute('rx', this.border_r);
         this.group.appendChild(cell);
+
+        //添加事件
+        this.addEvent(cell)
+        this.object = cell
+    }
+
+    addEvent(cell) {
+
+        cell.addEventListener('mousemove', (event) => {
+            if (event.buttons === 1) {
+                this.x += event.movementX;
+                this.y += event.movementY;
+                this.object.setAttribute('x', this.x);
+                this.object.setAttribute('y', this.y);
+            }
+        })
+
     }
 
 }
--- src/test.mjs
+++ src/test.mjs
@@ -1,6 +1,6 @@
 import {Cell} from "./api/cell.mjs";
 
-let cell = new Cell(10,10,100,100);
+let cell = new Cell(30,10,300,300, '#254a75',2,'24px');
 let root = document.createElementNS("http://www.w3.org/2000/svg", 'svg');
 root.setAttribute('width', 800);
 root.setAttribute('height', 800);
@@ -8,4 +8,3 @@ document.body.appendChild(root)
 cell.setGroup(root)
 cell.draw()
 
-console.log(1231)
